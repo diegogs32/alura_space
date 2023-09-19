bce42c45f30289e879304340989578d73a04b59f
Mario Venancio <nf.mariovenanciomvfs@fab.mil.br>
2023-09-19 17:30:12 -0300

    Criando Galeria de Fotos dentro do Admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,7 +3,10 @@ from galeria.models import Fotografia
 # Register your models here.
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    lista_per_page = 10
 admin.site.register(Fotografia, ListandoFotografias)
