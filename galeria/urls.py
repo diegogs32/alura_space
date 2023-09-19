39fd8b0e3624001ce18ac593c980414324677c87
Mario Venancio <nf.mariovenanciomvfs@fab.mil.br>
2023-09-19 10:17:49 -0300

    Fim da aula criando um template

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from galeria.views import index, imagem
 
+
+
 urlpatterns = [
      path('', index, name='index'),
      path('imagem/', imagem, name='imagem'),
+    
 
 ]
